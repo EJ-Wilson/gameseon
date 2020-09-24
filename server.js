9c148063c8d8e8cce688be1b96f6fbd2507597c0
EJ-Wilson <edward.j.wilson@hotmail.com>
2020-09-24 18:21:48 +0100

    Port fix

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const app = express(),
             DIST_DIR = path.join(__dirname, 'dist'),
             HTML_FILE = path.join(DIST_DIR, 'index.html')
 app.use(express.static(DIST_DIR))
-app.get('/', (req, res) => {
-    res.sendFile(HTML_FILE)
-})
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`App listening to ${process.env.PORT || 5000}....`)
     console.log('Press Ctrl+C to quit.')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(HTML_FILE)
 })
 
 app.post("/projects", function(request, response) {
